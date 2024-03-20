c27829efe173b469168b5446833c018a5788ace1
Brice Kelly <ntirukelly@gmail.com>
2024-03-20 16:20:26 +0200

    new-commit

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ const menu = document.querySelector('#menu');
     menu.classList.toggle('hidden')
     myMenu.classList.toggle('bg-white')
 })
+window.addEventListener('DOMContentLoaded', () => {
+    const skillCards = document.querySelectorAll('.skill-card');
+    skillCards.forEach((card, index) => {
+      card.style.animationDelay = `${index * 200}ms`;
+      card.classList.add('animate-fadeInUp');
+    });
+  });
